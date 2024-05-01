ef28179209b7512c8c8ac1656a4a1d53fc927615
simplousss <knappr@wsc.school.nz>
2024-05-02 10:22:14 +1200

    created initial quiz

---
+++ main.py
@@ -0,0 +1,17 @@
+# Ask the user their name and store it
+name = input("What is your name?")
+
+# Greet the user and introduce the quiz
+print("Hello welcome to my phython quiz,", name)
+
+# Introduce what the quiz is about
+print("This quiz is about the programming language python")
+
+# Ask the user a question
+input("What symbol is used to denote comments in Python a: // b: # c: /* */ d: -- ?")
+
+# Tell them the correct answer
+print("The correct anwser is #")
+
+# End the quiz
+print("This is the end of the quiz thank you for playing")

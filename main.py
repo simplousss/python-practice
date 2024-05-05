9909880132b3daadc87ef16202a60e2ba782fb9c
simplousss <knappr@wsc.school.nz>
2024-05-06 11:59:15 +1200

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,30 @@
 name = input("What is your name?")
 
 # Greet the user and introduce the quiz
-print("Hello welcome to my phython quiz,", name)
+print("Hello welcome to my phython quiz ",name)
 
 # Introduce what the quiz is about
-print("This quiz is about the programming language python")
+print("This quiz is about the programming language python.")
 
 # Ask the user a question
-input("What symbol is used to denote comments in Python a: // b: # c: /* */ d: -- ?")
+answer1 = input("1. What symbol is used to denote comments in Python a: // b: # c: /* */ d: -- ?")
 
-# Tell them the correct answer
-print("The correct anwser is")
+# Check the user’s answer and give feedback
+if answer1 == "#":
+     print("correct!")
+else:
+    print("Incorrect, the correct answer is #.") 
+
+# Ask the user a question
+answer2 = input("What year was python founded")
+
+# Check the user’s answer and give feedback
+if int(answer2) < 1991:
+     print("its an earleir date than that")
+elif int(answer2) > 1991: 
+     print("its a later date than that")
+else:
+          print("Correct!")
 
 # End the quiz
-print("This is the end of the quiz thank you for playing")
+print("This is the end of the quiz thank you for playing.")
